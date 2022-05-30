2bedbf957a601748283e2cbdd5e7e4402916c5f6
Shawon Sarowar <60030140+shawonNirob@users.noreply.github.com>
2022-05-30 20:07:24 +0600

    Create Betwise operator.java

---
+++ Betwise operator.java
@@ -0,0 +1,39 @@
+package operator;
+
+public class Shift {
+    public static void main(String[] args) {
+        int a = 5, b = 2;
+        int c = a>>b;
+        System.out.println(c);
+    }
+}
+
+package operator;
+
+public class Shift {
+    public static void main(String[] args) {
+        int a = 40, b = 3;
+        int c = a>>b;
+        System.out.println(c);
+    }
+}
+
+package operator;
+
+public class Shift {
+    public static void main(String[] args) {
+        int a = 40, b = 1;
+        int c = a>>b;
+        System.out.println(c);
+    }
+}
+
+package operator;
+
+public class Shift {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+        int c = a<<b;
+        System.out.println(c);
+    }
+}
